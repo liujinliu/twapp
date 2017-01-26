d81b83f0d2fa8b353c1c6a84b68ecc91ce2606d0
liujinliu <liujinliu@lbesec.com>
2017-01-26 12:17:54 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+all:
+	@echo "do nothing"
+
+clean:
+	rm -f `find . -type f -name '*.py[co]' `
+	rm -fr */*.egg-info build dist
+
+build: clean
+	python setup.py build_py bdist_wheel
+	cp Makefile dist
+
+install: build
+	pip install dist/*.whl -U
+
+uninstall:
+	pip uninstall twapp -y
+
+deploy:
+	pip install *.whl -U
+
+
+.PHONY : all clean build install
---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def _setup():
+    setup(
+        name='twapp',
+        version='0.0.1',
+        description='tornado web app generator',
+        url='',
+        install_requires=['tornado'],
+        packages=find_packages('src'),
+        include_package_data=True,
+        package_dir={'': 'src'},
+        package_data={
+            'twapp': [
+                'shelves/twapp/*',
+                'shelves/twapp/config/*',
+                'shelves/twapp/src/twapp/*',
+                'shelves/twapp/src/twapp/api/*',
+                'shelves/twapp/src/twapp/api/handlers/*',
+                'shelves/twapp/src/twapp/controllers/*',
+                'shelves/twapp/src/twapp/controllers/modle/*',
+            ]
+        },
+        entry_points={
+            'console_scripts': [
+                'twapp-make=twapp.main:main',
+                ]
+            },
+        classifiers=[
+            'Development Status :: 4 - Beta Development Status',
+            'Environment :: Console',
+            'Topic :: Utilities',
+        ],
+    )
+
+
+def main():
+    _setup()
+
+
+if __name__ == '__main__':
+    main()
